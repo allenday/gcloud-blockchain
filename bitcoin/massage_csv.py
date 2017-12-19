11c99aed565e6f435b29b77fb0088a8d4c2e7279
Allen Day <allenday@allenday.com>
2017-12-19 14:01:18 +0000

    json

--- bitcoin/massage_csv.py
+++ bitcoin/massage_csv.py
@@ -15,7 +15,8 @@ def double_sha256(s):
 #hash = '11b366edfc0a8b66feebae5c2e25a7b6a5d1cf31'
 #print hash_to_address("00".decode('hex'), hash.decode('hex'))
 
-print next(sys.stdin).rstrip()
+next(sys.stdin).rstrip()
+
 for line in sys.stdin:
     fields = line.rstrip().split("\t")
 
@@ -34,5 +35,5 @@ for line in sys.stdin:
     oaddr = map(lambda i: '"'+hash_to_address('00'.decode('hex'), i.decode('hex'))+'"', ohash)
     fields[10] = '['+(','.join(oaddr))+']'
 
-    print '{"block_version":'+fields[0]+',"block_time":"'+fields[1]+',"block_height":'+fields[2]+',"block_hash":"'+fields[3]+'"'+',"tx_version":'+fields[4]+',"tx_hash":"'+fields[5]+'"'+',"i_pos":'+fields[6]+',"i_pubkey_hash":'+fields[7]+',"i_value":'+fields[8]+',"o_pos":'+fields[9]+',"o_pubkey_hash":'+fields[10]+',"o_value":'+fields[11]+'}'
+    print '{"block_version":'+fields[0]+',"block_time":"'+fields[1]+'","block_height":'+fields[2]+',"block_hash":"'+fields[3]+'"'+',"tx_version":'+fields[4]+',"tx_hash":"'+fields[5]+'"'+',"i_pos":'+fields[6]+',"i_pubkey_hash":'+fields[7]+',"i_value":'+fields[8]+',"o_pos":'+fields[9]+',"o_pubkey_hash":'+fields[10]+',"o_value":'+fields[11]+'}'
 
