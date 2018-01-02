b57532133c2a4aac2d6c625375d6eb19ec06efdd
Allen Day <allenday@allenday.com>
2018-01-02 02:44:43 +0000

    load to bq

--- bitcoin/generate_csv.bash
+++ bitcoin/generate_csv.bash
@@ -2,11 +2,15 @@
 
 mkdir -p csv
 
-for Y in 2009 2010 2011 2012 2013; do
+for Y in 2009 2010 2011 2012 2013 ; do
 	echo $Y; 
 	bash abe_to_csv.bash $USER $PASS $Y-01-01 $Y-03-01		| python massage_csv.py > csv/BTC-$Y-Q1.csv;
+	bq load --replace --source_format NEWLINE_DELIMITED_JSON BTC_blockchain.tx${Y}Q1 csv/BTC-$Y-Q1.csv BTC_blockchain.schema.json
 	bash abe_to_csv.bash $USER $PASS $Y-03-01 $Y-06-01 		| python massage_csv.py > csv/BTC-$Y-Q2.csv; 
+	bq load --replace --source_format NEWLINE_DELIMITED_JSON BTC_blockchain.tx${Y}Q2 csv/BTC-$Y-Q2.csv BTC_blockchain.schema.json
 	bash abe_to_csv.bash $USER $PASS $Y-06-01 $Y-09-01 		| python massage_csv.py > csv/BTC-$Y-Q3.csv; 
+	bq load --replace --source_format NEWLINE_DELIMITED_JSON BTC_blockchain.tx${Y}Q3 csv/BTC-$Y-Q3.csv BTC_blockchain.schema.json
 	bash abe_to_csv.bash $USER $PASS $Y-01-01 $(($Y + 1))-01-01	| python massage_csv.py > csv/BTC-$Y-Q4.csv; 
+	bq load --replace --source_format NEWLINE_DELIMITED_JSON BTC_blockchain.tx${Y}Q4 csv/BTC-$Y-Q4.csv BTC_blockchain.schema.json
 done
 
